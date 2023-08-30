d63d4375029493a70f3855c96ebf0d3f074567ff
Raulcasallas26 <rcasallas26@gmail.com>
2023-08-30 16:54:33 -0500

    prueba

--- Drive_bakend/models/server.js
+++ Drive_bakend/models/server.js
@@ -27,6 +27,7 @@ class Server {
     middlewares() {
         this.app.use(express.json())
         this.app.use(cors())
+        this.app.use(express.static('public'))
     }
     escuchar() {
         this.app.listen(process.env.PORT, () => {
